0d925795113e4d7d8eade0018efa15289f2aa7ee
Kostya Kostikov <kostickov.costya@yandex.ru>
2026-01-22 00:15:25 +0300

    add: salad for tests

---
+++ salad/modules/transformer.py
@@ -0,0 +1,59 @@
+"""This module implements some basic transformer components."""
+
+from typing import Optional
+
+import jax
+import jax.numpy as jnp
+import haiku as hk
+
+from salad.modules.basic import MLP, init_relu
+
+def resi_dual(local, incremental, output):
+    """ResiDual update (mixed pre-norm and post-norm)."""
+    local = hk.LayerNorm([-1], True, True)(local + output)
+    incremental = incremental + output
+    return local, incremental
+
+def resi_dual_input(features):
+    """Extract the input part from a set of ResiDual features."""
+    return features[0]
+
+def prenorm_skip(local, output):
+    return local + output
+
+def postnorm_skip(local, output):
+    return hk.LayerNorm([-1], True, True)(local + output)
+
+def prenorm_input(local):
+    return hk.LayerNorm([-1], True, True)(local)
+
+def drop(data, p=0.1, is_training=True):
+    """Dropout"""
+    if is_training:
+        mask = jax.random.bernoulli(
+            hk.next_rng_key(), p, data.shape)
+        data = jnp.where(mask, 0, data) / (1 - p)
+    return data
+
+class Transition(hk.Module):
+    """A simple MLP feed forward block."""
+    def __init__(self, factor=4, depth=2,
+                 activation=jax.nn.relu,
+                 final_init="zeros",
+                 name: Optional[str] = "transition"):
+        super().__init__(name)
+        self.activation = activation
+        self.final_init = final_init
+        self.factor = factor
+        self.depth = depth
+
+    def __call__(self, inputs):
+        normalized = hk.LayerNorm(axis=[-1], create_scale=True, create_offset=True)(inputs)
+        update = MLP(
+            inputs.shape[-1] * self.factor,
+            out_size=inputs.shape[-1],
+            activation=self.activation,
+            depth=self.depth, init=init_relu(),
+            final_init=self.final_init)(normalized)
+        return update
+
